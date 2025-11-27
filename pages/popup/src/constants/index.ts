89bfd045b7abb535bd9fba78713b32b30c8f8796
cnohall <christopher.nohall@gmail.com>
2025-11-27 15:18:14 +0900

    Improve error messaging on set password screen

---
+++ pages/popup/src/constants/index.ts
@@ -0,0 +1,9 @@
+export const ERRORS = {
+  PLEASE_ACCEPT_TERMS: 'Please accept the terms.',
+  PASSWORDS_DO_NOT_MATCH: 'Passwords do not match.',
+  PLEASE_CHOOSE_STRONGER_PASSWORD: 'Please choose a stronger password.',
+  PASSWORD_TOO_SHORT: 'Password must be at least 8 characters long.',
+  PASSWORD_REQUIRED: 'Password is required.',
+};
+
+export const MIN_PASSWORD_LENGTH = 8;
