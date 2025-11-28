88456a9f9af1ac03dffac8be57c5706e9c51feae
cnohall <christopher.nohall@gmail.com>
2025-11-28 12:16:55 +0900

    remove ERROR_MESSAGES from previous PR

--- pages/popup/src/constants/index.ts
+++ pages/popup/src/constants/index.ts
@@ -1,12 +1,5 @@
 export const ERROR_MESSAGES = {
-  PLEASE_ACCEPT_TERMS: 'Please accept the terms.',
-  PASSWORDS_DO_NOT_MATCH: 'Passwords do not match.',
-  PLEASE_CHOOSE_STRONGER_PASSWORD: 'Please choose a stronger password.',
-  PASSWORD_TOO_SHORT: 'Password must be at least 8 characters long.',
-  PASSWORD_REQUIRED: 'Password is required.',
   INCORRECT_PASSWORD: 'Incorrect password.',
   SOMETHING_WENT_WRONG: 'Something went wrong. Please try again.',
   PLEASE_ENTER_PASSWORD: 'Please enter your password.',
 };
-
-export const MIN_PASSWORD_LENGTH = 8;
