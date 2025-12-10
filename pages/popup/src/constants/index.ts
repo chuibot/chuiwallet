a5568c4f5a8ad4033cb4e7d2676ddcb9a17c1cce
cnohall <christopher.nohall@gmail.com>
2025-12-10 16:29:35 +0900

    Improve error handling

--- pages/popup/src/constants/index.ts
+++ pages/popup/src/constants/index.ts
@@ -3,4 +3,7 @@ export const ERROR_MESSAGES = {
   SOMETHING_WENT_WRONG: 'Something went wrong. Please try again.',
   PLEASE_ENTER_PASSWORD: 'Please enter your password.',
   SOMETHING_WENT_WRONG_WHILE_SENDING_TRANSACTION: 'An error occurred while sending the transaction.',
+  SEND_TRANSACTION_TOO_SMALL: 'Transaction amount is too small. Please increase the amount.',
+  SEND_TRANSACTION_INSUFFICIENT_FUNDS: 'Insufficient funds to complete this transaction.',
+  SEND_TRANSACTION_FEE_ERROR: 'Transaction fee error. Please try adjusting the fee.',
 };
