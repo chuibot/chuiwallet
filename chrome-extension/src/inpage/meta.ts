6b495b47883cd3fec849314b5a468860673bdd8a
Vin Lim <vin.lim.yh@gmail.com>
2026-05-09 13:16:30 +0800

    [Audit] CHUI-AUDIT-028

--- chrome-extension/src/inpage/meta.ts
+++ chrome-extension/src/inpage/meta.ts
@@ -3,5 +3,7 @@ import type { BtcProviderInfo } from '@src/inpage/types';
 export const providerInfo: BtcProviderInfo = Object.freeze({
   id: 'ChuiWalletProvider',
   name: 'Chui Wallet',
+  webUrl: 'https://chuiwallet.com/',
+  chromeWebStoreUrl: 'https://chromewebstore.google.com/detail/...', //To be updated upon approval
   methods: Object.freeze(['getXpub', 'getAddresses', 'getXpubAddresses'] as const),
 });
